5e04e241fa9c4351ccd61d28f2cf73210147b475
Hamza Naveed <hamza@hamzas-mbp.mynet>
2020-03-31 05:08:55 +0100

    🚀 CSS Changes; exir exit close z

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,46 +1,46 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
+   render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+      if (this.props.data) {
+         var name = this.props.data.name;
+         var occupation = this.props.data.occupation;
+         var description = this.props.data.description;
+         var city = this.props.data.address.city;
+         var networks = this.props.data.social.map(function (network) {
+            return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+         })
+      }
 
-    return (
-      <header id="home">
+      return (
+         <header id="home">
 
-      <nav id="nav-wrap">
+            <nav id="nav-wrap">
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+               <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+               <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-      </nav>
+            </nav>
 
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline animated fadeIn delay-2s "> {name}</h1>
-            <h3 className=" animated fadeIn delay-3s">I'm a {city} based <span>{occupation}</span>. {description}.</h3>
-            <hr />
-            <ul className="social animated fadeIn delay-4s">
-               {networks}
-            </ul>
-         </div>
-      </div>
+            <div className="row banner">
+               <div className="banner-text">
+                  <h1 className="responsive-headline fadeIn  "> {name}</h1>
+                  <h3 className="  fadeIn delay-3s">I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+                  <hr />
+                  <ul className="social  fadeIn delay-4s">
+                     {networks}
+                  </ul>
+               </div>
+            </div>
 
-      <p className="scrolldown animated fadeIn delay-5s">
-         <a className="smoothscroll" href=""><i className="fa fa-heart"></i></a>
-      </p>
+            <p className="scrolldown  fadeIn delay-5s">
+               <a className="smoothscroll" href=""><i className="fa fa-heart"></i></a>
+            </p>
 
-   </header>
-    );
-  }
+         </header>
+      );
+   }
 }
 
 export default Header;
