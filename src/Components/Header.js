d8b813cb3c1d1c463dc54780c832987d7e4930cc
Hamza Naveed <hamza@hamzas-mbp.mynet>
2020-06-27 04:26:58 +0100

    New Changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,45 +1,51 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-   render() {
+  render() {
+    if (this.props.data) {
+      var name = this.props.data.name;
+      var occupation = this.props.data.occupation;
+      var description = this.props.data.description;
+      var city = this.props.data.address.city;
+      var english = this.props.data.english;
+      var networks = this.props.data.social.map(function (network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
+    }
 
-      if (this.props.data) {
-         var name = this.props.data.name;
-         var occupation = this.props.data.occupation;
-         var description = this.props.data.description;
-         var city = this.props.data.address.city;
-         var networks = this.props.data.social.map(function (network) {
-            return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-         })
-      }
-
-      return (
-         <header id="home">
-
-            <nav id="nav-wrap">
-
-               <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-               <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-            </nav>
-
-            <div className="row banner">
-               <div className="banner-text">
-                  <h1 className="responsive-headline fadeIn  "> {name}</h1>
-                  <h3 className="  fadeIn delay-3s">I'm a {city} based <span>{occupation}</span>. {description}.</h3>
-                  <hr />
-                  <ul className="social ">
-                     {networks}
-                  </ul>
-               </div>
-            </div>
-            <p className="scrolldown  fadeIn delay-5s">
-               <a className="smoothscroll" href=""><i className="fa fa-heart"></i></a>
-            </p>
-
-         </header>
-      );
-   }
+    return (
+      <header id='home'>
+        <div className='row banner'>
+          <div className='banner-text'>
+            <h1 className='responsive-headline fadeIn urduname' id='nameurdu'>
+              {' '}
+              {name}
+            </h1>
+            <h1 className='responsive-headline fadeIn' id='name_english'>
+              {' '}
+              {english}
+            </h1>
+            <h3 className='  fadeIn delay-3s'>
+              I'm a {city} based <span>{occupation}</span>. {description}.
+            </h3>
+            <hr />
+            <ul className='social '>{networks}</ul>
+          </div>
+        </div>
+        <p className='scrolldown  fadeIn delay-5s'>
+          <a className='smoothscroll' href=''>
+            <i className='fa fa-heart'></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
 }
 
 export default Header;
